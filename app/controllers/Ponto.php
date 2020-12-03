fbdd633b2e2dc672cd01fe52a87c59aa9ce5f20f
jhuliocastro <jhuliocastro@hotmail.com>
2020-12-02 23:19:04 -0300

    PONTO ELETRÔNICO

--- app/controllers/Ponto.php
+++ app/controllers/Ponto.php
@@ -15,16 +15,48 @@ class Ponto extends Controller{
         $funcionarios = new FuncionariosModel();
         $dadosFuncionarios = $funcionarios->lista();
         $horarios = null;
+        $i = 0;
         foreach($dadosFuncionarios as $d){
+            if($i == 0){
+                $horarios .= "<div class='row'>";
+            }
             $horarios .= "
-                <div class='form-group'>
-                    <fieldset class=\"fild\">
-                        $d->nome
-                    </fieldset>
-                    <br/>
-                    <input name='txt_$d->nome' data-role=\"timepicker\" data-seconds=\"false\">
+                <div class='col-md-6'>
+                    <div class='card' style='padding: 5px;'>
+                        <div class='card-header'>
+                            <span style='font-weight: bold; font-size: 18px;'>$d->nome</span>
+                        </div>
+                        <div class='card-body'>
+                            <form method='post' action=''>
+                                <fieldset class=\"fild\">
+                                    Horário Chegada
+                                </fieldset>
+                                <input data-value='{{hora_chegada_$d->nome}}' name='txt_hora_chegada_$d->nome' data-role=\"timepicker\" data-seconds=\"false\">
+                                <br/>
+                                <fieldset class=\"fild\">
+                                    Horário Saída Almoço
+                                </fieldset>
+                                <input data-value='{{hora_saida_almoco_$d->nome}}' name='txt_hora_saida_almoco_$d->nome' data-role=\"timepicker\" data-seconds=\"false\">
+                                <br/>
+                                <fieldset class=\"fild\">
+                                    Horário Chegada Almoço
+                                </fieldset>
+                                <input data-value='{{hora_chegada_almoco_$d->nome}}' name='txt_hora_chegada_almoco_$d->nome' data-role=\"timepicker\" data-seconds=\"false\">
+                                <br/>
+                                <fieldset class=\"fild\">
+                                    Horário Saída
+                                </fieldset>
+                                <input data-value='{{hora_saida_$d->nome}}' name='txt_hora_saida_$d->nome' data-role=\"timepicker\" data-seconds=\"false\">
+                                <br/>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             ";
+            $i++;
+            if($i == 2){
+                $horarios .= "</div>";
+            }
         }
         parent::render("ponto", "horarios", [
             "horarios" => $horarios
